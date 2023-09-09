984c0fa18a6a19a0c0a23e4eb6806f87105701ba
Umeshan Weerasekara <UmeshanUC@gmail.com>
2023-09-09 16:52:19 +0530

    db conn string changed

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,21 +1,18 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(
-      "mongodb+srv://muzny:123@Bcas@cluster0.nsoja.mongodb.net/",
-      {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true,
-      }
-    );
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
-  } catch (error) {
-    console.error(`Error: ${error.message}`.red.underline.bold);
-    process.exit(1);
-  }
+	try {
+		const conn = await mongoose.connect('mongodb+srv://razny:123@Bcas@cluster0.nsoja.mongodb.net/', {
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+			useUnifiedTopology: true,
+		});
+		console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+	} catch (error) {
+		console.error(`Error: ${error.message}`.red.underline.bold);
+		process.exit(1);
+	}
 };
 
 export default connectDB;
